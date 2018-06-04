5656b1fef71b292a4431063ebf51ff6eda114e30
carnd <p.liu@gotion.local>
2018-06-04 12:58:43 +0800

    add some coments in model.py

--- model.py
+++ model.py
@@ -84,9 +84,8 @@ train_generator = generator(train_samples, batch_size=32)
 valid_generator = generator(valid_samples, batch_size=32)
 gc.collect()
 
-# define the Net
 
-
+# define the Deep Learning pipeline
 model = Sequential()
 model.add(Lambda(lambda x: x/255-0.5,input_shape=(160,320,3)))
 model.add(Cropping2D(cropping=((70,20),(0,0))))
@@ -100,6 +99,8 @@ model.add(Dense(100))
 model.add(Dense(50))
 model.add(Dense(10))
 model.add(Dense(1))
+
+# Train and vlidation
 
 model.compile(loss='mse', optimizer='adam')
 history_object = model.fit_generator(train_generator, samples_per_epoch=len(train_samples),
